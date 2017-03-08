bb15d0974f3492da1ef28f10c322ce9f52354e01
P0G8HNSKR2C0O8B\Administrator <kitsada_tang@cmu.ac.th>
2017-03-08 17:37:17 +0700

    Update Currency class

---
+++ src/main/java/utility/Currency.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 08/03/2017.
+ */
+package utility;
+public class Currency {
+    public double exchange(String sourceCurrency,double amount,String targetCurrency){
+        return 0.0;
+    }
+}
